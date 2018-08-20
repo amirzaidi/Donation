7d7b1d832bfa948b24c01eccf51773f29a755933
Amir Zaidi <azaidi@live.nl>
2018-08-20 18:51:45 +0200

    Add files

---
+++ app/src/main/java/amirz/donation/DonationActivity.java
@@ -0,0 +1,70 @@
+package amirz.donation;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendTelegram();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_donation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_telegram) {
+            sendTelegram();
+            return true;
+        } else if (id == R.id.action_mail) {
+            sendMail();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendTelegram() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://t.me/mirai"));
+        startActivity(intent);
+    }
+
+    private void sendMail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        String uriText = "mailto:" + Uri.encode("azaidi@live.nl") +
+                "?subject=" + Uri.encode("Hi Amir") +
+                "&body=" + Uri.encode("What's up?");
+        intent.setData(Uri.parse(uriText));
+        startActivity(intent);
+    }
+}
