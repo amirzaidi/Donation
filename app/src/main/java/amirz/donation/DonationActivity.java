928a60c69060698c85c28b2f2a231fcfe4fd7bfc
Amir Zaidi <azaidi@live.nl>
2018-08-20 23:15:21 +0200

    Begin implementing in app purchases

--- app/src/main/java/amirz/donation/DonationActivity.java
+++ app/src/main/java/amirz/donation/DonationActivity.java
@@ -3,14 +3,31 @@ package amirz.donation;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class DonationActivity extends AppCompatActivity {
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DonationActivity extends AppCompatActivity
+        implements PurchasesUpdatedListener, BillingClientStateListener, SkuDetailsResponseListener {
+    private static final String TAG = "DonationActivity";
+    private BillingClient mBilling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +36,66 @@ public class DonationActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final List<String> skuList = new ArrayList<>();
+        skuList.add("donate.regular");
+        skuList.add("donate.large");
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (mBilling.isReady()) {
+                    mBilling.querySkuDetailsAsync(SkuDetailsParams.newBuilder()
+                            .setSkusList(skuList)
+                            .setType(BillingClient.SkuType.INAPP)
+                            .build(), DonationActivity.this);
+                } else {
+                    Snackbar.make(view, "Connection to Google Play failed", Snackbar.LENGTH_LONG)
+                            .setAction("Reconnect", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    mBilling.startConnection(DonationActivity.this);
+                                }
+                            }).show();
+                }
             }
         });
+
+        BillingClient.Builder builder = BillingClient.newBuilder(this);
+        builder.setListener(this);
+        mBilling = builder.build();
+
+        mBilling.startConnection(this);
+    }
+
+    // Connected
+    @Override
+    public void onBillingSetupFinished(int responseCode) {
+        Log.w(TAG, "onBillingSetupFinished " + responseCode);
+    }
+
+    // Start purchase
+    @Override
+    public void onSkuDetailsResponse(int responseCode, List<SkuDetails> skuDetailsList) {
+        Log.w(TAG, "onSkuDetailsResponse " + responseCode + " " + skuDetailsList.size());
+    }
+
+    // Purchased
+    @Override
+    public void onPurchasesUpdated(int responseCode, @Nullable List<Purchase> purchases) {
+        Log.w(TAG, "onPurchasesUpdated " + responseCode);
+    }
+
+    // Disconnected
+    @Override
+    public void onBillingServiceDisconnected() {
+        Log.w(TAG, "onBillingServiceDisconnected");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBilling.endConnection();
     }
 
     @Override
